2b955020018a5eeb2d22ce74d4816595d5dba83c
pojthi <pojthi@gmail.com>
2024-11-03 19:17:56 +0700

    Update React and MSSQL

--- apiGestores/apiGestores/Migrations/20241103104504_mssql.local_migration_529.Designer.cs
+++ apiGestores/apiGestores/Migrations/20241103104504_mssql.local_migration_529.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using apiGestores.Context;
 
 namespace apiGestores.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241103104504_mssql.local_migration_529")]
+    partial class mssqllocal_migration_529
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ apiGestores/apiGestores/Migrations/20241103104504_mssql.local_migration_529.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace apiGestores.Migrations
+{
+    public partial class mssqllocal_migration_529 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderDate",
+                table: "foods");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "foods");
+
+            migrationBuilder.DropColumn(
+                name: "UnitPrice",
+                table: "foods");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Order_Date",
+                table: "foods",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Total_Price",
+                table: "foods",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Unit_Price",
+                table: "foods",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order_Date",
+                table: "foods");
+
+            migrationBuilder.DropColumn(
+                name: "Total_Price",
+                table: "foods");
+
+            migrationBuilder.DropColumn(
+                name: "Unit_Price",
+                table: "foods");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OrderDate",
+                table: "foods",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "foods",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "UnitPrice",
+                table: "foods",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
